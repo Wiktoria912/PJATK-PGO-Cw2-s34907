b7fd09948dac4decb3e8a4c4a9aa375e294d2b6d
Wiktoria <wikidryja@gmail.com>
2026-03-30 18:03:27 +0200

    Dodanie klasy Ksiazka.java

---
+++ src/Ksiazka.java
@@ -0,0 +1,30 @@
+public class Ksiazka {
+  
+    private String tytul;
+    private String autor;
+    private int liczbaStron;
+    private boolean dostepna;
+
+    public Ksiazka(String tytul, String autor, int liczbaStron, boolean dostepna) {
+        this.tytul = tytul;
+        this.autor = autor;
+        this.liczbaStron = liczbaStron;
+        this.dostepna = dostepna;
+    }
+    public void wypiszInfo() {
+        System.out.println("Książka: " + tytul + " | Autor: " + autor +
+                " | Stron: " + liczbaStron + " | Dostępna: " + dostepna);
+    }
+    public void wypozycz() {
+        this.dostepna = false;
+    }
+    public void zwroc() {
+        this.dostepna = true;
+    }
+    public String getTytul() {
+        return tytul;
+    }
+    public boolean isDostepna() {
+        return dostepna;
+    }
+}
